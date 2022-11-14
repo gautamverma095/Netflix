ddf54dd902663019bd08d7a832f47142453b9b57
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-11-15 00:47:55 +0530

    done

--- src/index.js
+++ src/index.js
@@ -4,39 +4,33 @@ import ReactDOM from "react-dom"
 import Card from "./Card"
 import "./index.css"
 import Sdata from "./Sdata"
+
+// function nCard(el,index,arr){
+// return (
+//   <Card
+//     imageLink={el.imageLink}
+//     name={el.name}
+//     sLink={el.sLink}
+//     title={el.title}
+//   />
+// );
+// }
+
 ReactDOM.render(
   <>
     <h1 className="heading_style">List of top 5 Netflix Series</h1>
-    <Card
-      imageLink={Sdata[0].imageLink}
-      name={Sdata[0].name}
-      sLink={Sdata[0].sLink}
-      title={Sdata[0].title}
-    />
-    <Card
-      imageLink={Sdata[1].imageLink}
-      name={Sdata[1].name}
-      sLink={Sdata[1].sLink}
-      title={Sdata[1].title}
-    />
-    <Card
-      imageLink={Sdata[2].imageLink}
-      name={Sdata[2].name}
-      sLink={Sdata[2].sLink}
-      title={Sdata[2].title}
-    />
-    <Card
-      imageLink={Sdata[3].imageLink}
-      name={Sdata[3].name}
-      sLink={Sdata[3].sLink}
-      title={Sdata[3].title}
-    />
-    <Card
-      imageLink={Sdata[2].imageLink}
-      name={Sdata[2].name}
-      sLink={Sdata[2].sLink}
-      title={Sdata[2].title}
-    />
+  
+    {Sdata.map((el) => {   
+          return (
+  <Card
+    imageLink={el.imageLink}
+    name={el.name}
+    sLink={el.sLink}
+    title={el.title}
+  />
+);
+    })}
+   
   </>,
   document.getElementById("root")
 );
